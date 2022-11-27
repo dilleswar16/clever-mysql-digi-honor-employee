a63cd3cf415d893c018fb552c7ddae987cc7e279
dilleswar16 <102014138+dilleswar16@users.noreply.github.com>
2022-11-27 14:21:48 +0530

    Add files via upload

---
+++ src/main/java/com/bos/employee/management/controller/EmployeeController.java
@@ -0,0 +1,63 @@
+package com.bos.employee.management.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bos.employee.management.model.Employee;
+import com.bos.employee.management.service.EmployeeService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeService employeeService;
+	
+	@GetMapping("/getallemployees")
+	public List<Employee> getAllEmployessForAdmin() {
+		System.out.println("In controller");
+		List<Employee> employees = employeeService.getAllEmployeesForAdmin();
+		return employees;
+	}
+
+	@GetMapping("/getemployee/{id}")
+	public Optional<Employee> getEmployessForAdmin(@PathVariable long id) {
+		Optional<Employee> employee = employeeService.getIndividualEmployee(id);
+		return employee;
+	}
+	
+	@PostMapping("/addemployee")
+	public Employee saveEmployeeToDB(@RequestBody Employee emp) {
+		//save student in DB
+		Employee employee = employeeService.addEmployeeToDB(emp);
+		return employee;
+
+	}
+	
+	@PatchMapping("/updateemployee/{id}")
+	public Employee updateEmployee(@PathVariable long id,@RequestBody Employee emp) {
+		Employee employee = employeeService.updateEmployee(id, emp);
+		return employee;
+		
+	}
+	
+	@DeleteMapping("/deleteemployee/{id}")
+	public String deleteEmployee(@PathVariable long id) {
+		employeeService.deleteEmployee(id);
+		return "Successfully Deleted";
+	}
+	
+	
+	
+	
+}
